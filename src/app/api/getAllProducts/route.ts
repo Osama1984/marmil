925075d733d9a12ece76d05ef04e9003d97cd01c
Osamah Neamah <oneamah@gmail.com>
2024-11-12 21:24:33 -0500

    change

--- src/app/api/getAllProducts/route.ts
+++ src/app/api/getAllProducts/route.ts
@@ -1,50 +1,43 @@
 import connectDB from '@/app/config/connection';
 import Product from '@/app/models/Product';
-import { cookies } from 'next/headers';
-import { NextRequest, NextResponse } from 'next/server';
+import User from '@/app/models/User';
+import { NextRequest } from 'next/server';
 
-export async function GET(req:any, res:any) {
+export async function GET(request: NextRequest) {
   try {
-   // Set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', 'https://marmil.vercel.app'); // Allow your frontend's domain
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-  // Handle preflight requests (OPTIONS)
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
     // Get pagination parameters from query string (default to page 1 and 10 items per page)
-    const url = new URL(req.url);
+    const url = new URL(request.url);
     const page = parseInt(url.searchParams.get('page') || '1', 10);
-    const limit = parseInt(url.searchParams.get('limit') || '2', 10);  // default to 2 items per page
+    const limit = parseInt(url.searchParams.get('limit') || '2', 4);
     const skip = (page - 1) * limit;
 
     // Connect to the database
+    const user = await User;
     await connectDB();
 
-    // Fetch products with pagination, and populate the 'user' field with selected user details
-    const products = await Product.find()
-      .populate('user', 'username email profileImage address phone state zipCode')
+    // Fetch products with pagination
+    const products = await Product.find().populate('user', 'username email profileImage address phone state zipCode')
       .skip(skip)
       .limit(limit);
 
     // Get the total count of products
     const totalProducts = await Product.countDocuments();
 
-    // Return paginated data in the response
-    return NextResponse.json({
-      success: true,
-      products,
-      totalProducts,
-      totalPages: Math.ceil(totalProducts / limit),
-      currentPage: page,
-    });
+    // Return paginated data
+    return new Response(
+      JSON.stringify({
+        success: true,
+        products,
+        totalProducts,
+        totalPages: Math.ceil(totalProducts / limit),
+        currentPage: page,
+      }),
+      { status: 200 }
+    );
   } catch (error) {
     console.error('Error fetching products:', error);
-    // Return an error response
-    return NextResponse.json(
-      { success: false, message: 'Failed to fetch products.', error: error },
+    return new Response(
+      JSON.stringify({ success: false, message: 'Failed to fetch products.' }),
       { status: 500 }
     );
   }
